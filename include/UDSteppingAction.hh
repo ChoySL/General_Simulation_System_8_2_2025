a03c999ccd191f6c0488ace25985b54720066cdb
ChoySL <nararaven3776@link.cuhk.edu.hk>
2025-02-11 19:45:23 +0800

    Fixing

--- include/UDSteppingAction.hh
+++ include/UDSteppingAction.hh
@@ -6,7 +6,7 @@
 #include "globals.hh"
 #include "UDInCodeControllingSurface.hh"
 # define M_PI 3.14159265358979323846
-// Forward declaration to avoid circular dependency
+
 class UDEventAction;
 
 class UDSteppingAction : public G4UserSteppingAction {
@@ -18,7 +18,6 @@ public:
 
 private:
     UDInCodeControllingSurface ControllingSurface;	
-// Pointer to EventAction for storing angles
 };
 
 #endif  // UDSTEPPINGACTION_HH
